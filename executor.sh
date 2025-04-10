1e05504b1a34f650011b97d967f6c13bc4f28241
byimam2nd <52017796+byimam2nd@users.noreply.github.com>
2025-04-10 09:34:07 +0700

    Update executor.sh

--- executor.sh
+++ executor.sh
@@ -24,7 +24,7 @@ fi
 
 # Ambil GitHub Token
 echo "[INFO] Mengambil GitHub Token dari: $TOKEN_FILE_URL"
-GITHUB_TOKEN=$(curl -s "$TOKEN_FILE_URL" | sed -n 's/^GITHUB_TOKEN="\?\(.*\)"\?/\1/p')
+GITHUB_TOKEN=$(curl -s "$TOKEN_FILE_URL"')
 echo $GITHUB_TOKEN
 
 if [[ -z "$GITHUB_TOKEN" ]]; then
