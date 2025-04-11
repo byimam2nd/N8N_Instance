92c5880f394655d66d1c2770f577f54059451ed8
byimam2nd <52017796+byimam2nd@users.noreply.github.com>
2025-04-11 13:09:05 +0700

    Update universal_lib_func.sh

--- universal_lib_func.sh
+++ universal_lib_func.sh
@@ -2,21 +2,6 @@
 # Universal Function
 # -------------------------------
 
-function sudo_controller() {
-  while true; do
-    read -p "Apakah ingin menggunakan sudo untuk setiap perintah? (y/n): " use_sudo
-    if [[ -z "$use_sudo" ]]; then
-      SUDO="sudo"
-      break
-    fi
-    case $use_sudo in
-      [Yy]*) SUDO="sudo"; break ;;
-      [Nn]*) SUDO=""; break ;;
-      *) echo "Masukkan y atau n saja!" ;;
-    esac
-  done
-}
-
 log() {
   if [ -n "$2" ]; then
     echo -e "$1$2${RESET}"
